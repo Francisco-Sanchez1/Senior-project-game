025514e82f70f0a7ff306aa24f697b0074e5a59c
CGonzalez08 <98342280+CGonzalez08@users.noreply.github.com>
2023-10-11 22:24:31 -0500

    transitionToCastleScenes

--- Baldemort/Assets/EnemySpawner.cs
+++ Baldemort/Assets/EnemySpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-
+    public Transform target;
     [SerializeField]
     private GameObject swarmerPrefab;
     [SerializeField]
@@ -14,19 +14,53 @@ public class EnemySpawner : MonoBehaviour
     private float swarmInterval = 3.5f;
     [SerializeField]
     private float archerSwarm = 10f;
-
+    public float detectionRadius;
+    public bool spawn;
+    public float spawnTimer;
+    public float spawnTimeFull = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(swarmInterval, swarmerPrefab));
+        target = GameObject.FindWithTag("Player").transform;
+    }
+    void FixedUpdate()
+    {
+        float distanceToPlayer = Vector2.Distance(target.position, transform.position);
+        
+        if (distanceToPlayer < detectionRadius)
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+        if (distanceToPlayer < detectionRadius && spawn == false)
+        {
+            spawnEnemy(swarmerPrefab);
+        }
+        if (spawnTimer <= 0 && distanceToPlayer < detectionRadius)
+        {
+            spawnTimer = spawnTimeFull;
+            spawn = false;
+        }
     }
 
+
     // Update is called once per frame
-    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    private void spawnEnemy(GameObject enemy)
     {
-        yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-6f, 6f), 0f), Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+        // Define the spawn radius around the EnemySpawner
+        float spawnRadius = 2.0f; // You can adjust this value to your desired radius.
+
+        // Generate random coordinates within the spawn radius
+        float randomX = transform.position.x + Random.Range(-spawnRadius, spawnRadius);
+        float randomY = transform.position.y + Random.Range(-spawnRadius, spawnRadius);
+
+        // Create a new Vector3 with the random coordinates
+        Vector3 spawnPoint = new Vector3(randomX, randomY, 0f);
+
+        // Offset the enemy's position if needed
+        // You can add/subtract values from the spawnPoint position as needed.
+
+        GameObject newEnemy = Instantiate(enemy, spawnPoint, Quaternion.identity);
+        spawn = true;
     }
 }
